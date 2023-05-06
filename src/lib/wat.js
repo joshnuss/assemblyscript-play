932ccaad4416df677dd7934eeaa7fe3a31d4b2e8
Josh Nussbaum <joshnuss@gmail.com>
2023-05-06 03:32:37 -0400

    Lint

--- src/lib/wat.js
+++ src/lib/wat.js
@@ -1,25 +1,24 @@
 import { simpleMode } from '@codemirror/legacy-modes/mode/simple-mode'
-import {StreamLanguage} from "@codemirror/language"
+import { StreamLanguage } from '@codemirror/language'
 
 // https://codemirror.net/5/demo/simplemode.html
 const old = simpleMode({
   start: [
     // string
-    {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string"},
+    { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string' },
 
     // keyword
-    {regex: /return|global|module|export|elem|type|local.get|table|memory|func|param|result/, token: "string"},
+    {
+      regex: /\b(return|global|module|export|elem|type|local.get|table|memory|func|param|result)\b/,
+      token: 'keyword'
+    },
 
     // number
-    {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: "number"},
+    { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: 'number' },
 
     // indent and dedent properties guide autoindentation
-    {regex: /([\(])/, indent: true},
-    {regex: /([\))]/, dedent: true},
-
-    // function name
-    {regex: /[\(](?:.*)\y/, token: "keyword"},
-
+    { regex: /([\(])/, indent: true },
+    { regex: /([\)])/, dedent: true },
   ]
 })
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,108 +1,100 @@
 <script>
-	import '../app.css';
-	import { onMount } from 'svelte';
-	import asc from 'assemblyscript/asc';
-	import CodeMirror from "svelte-codemirror-editor";
-  import { javascript } from "@codemirror/lang-javascript";
-	import { wat } from '$lib/wat'
+  import '../app.css'
+  import { onMount } from 'svelte'
+  import asc from 'assemblyscript/asc'
+  import CodeMirror from 'svelte-codemirror-editor'
+  import { javascript } from '@codemirror/lang-javascript'
+  import { wat } from '$lib/wat'
 
-	let source = `export function add(a: u32, b: u32): u32 {\n  return a + b;\n}`;
-	let result = null;
-	let compiling = false;
+  let source = `export function add(a: u32, b: u32): u32 {\n  return a + b;\n}`
+  let result = null
+  let compiling = false
 
-	$: error = result?.error;
+  $: error = result?.error
 
-	onMount(compile);
+  onMount(compile)
 
-	async function handleChange(e) {
-		source = e.detail;
-		compile();
-	}
+  async function handleChange(e) {
+    source = e.detail
+    compile()
+  }
 
-	async function compile() {
-		compiling = true;
-		result = await asc.compileString(source);
-		compiling = false;
-	}
+  async function compile() {
+    compiling = true
+    result = await asc.compileString(source)
+    compiling = false
+  }
 
-	function formattedErrors() {
-		return result.stderr.toString().split('\n');
-	}
+  function formattedErrors() {
+    return result.stderr.toString().split('\n')
+  }
 </script>
 
 <header>
-	AssemblyScript <em>Playground</em>
+  AssemblyScript <em>Playground</em>
 </header>
 
 <main>
-	<div class="pane">
-		<CodeMirror
-			value={source}
-			lang={javascript({ typescript: true })}
-			on:change={handleChange}
-			/>
-	</div>
+  <div class="pane">
+    <CodeMirror value={source} lang={javascript({ typescript: true })} on:change={handleChange} />
+  </div>
 
-	<div class="pane">
-		<CodeMirror
-			value={result?.text ?? ''}
-			lang={wat}
-			readonly
-		/>
-	</div>
+  <div class="pane">
+    <CodeMirror value={result?.text ?? ''} lang={wat} readonly />
+  </div>
 </main>
 
 {#if compiling || error}
-	<footer class:error>
-		{#if compiling}
-			Compiling...
-		{:else if error}
-			{#each formattedErrors() as error}
-				{@html error.replace(/ /g, '&nbsp;')}<br/>
-			{/each}
-		{/if}
-	</footer>
+  <footer class:error>
+    {#if compiling}
+      Compiling...
+    {:else if error}
+      {#each formattedErrors() as error}
+        {@html error.replace(/ /g, '&nbsp;')}<br />
+      {/each}
+    {/if}
+  </footer>
 {/if}
 
 <style>
-	main {
-		display: flex;
-		flex: auto;
-	}
-	main .pane {
-		font-family: var(--font-mono);
-		font-size: var(--font-size-2);
-		border-radius: 0;
-		resize: none;
-		flex: 1;
-	}
-	main .pane:first-of-type {
-		border-right: solid var(--border-size-2) var(--gray-4);
-	}
+  main {
+    display: flex;
+    flex: auto;
+  }
+  main .pane {
+    font-family: var(--font-mono);
+    font-size: var(--font-size-2);
+    border-radius: 0;
+    resize: none;
+    flex: 1;
+  }
+  main .pane:first-of-type {
+    border-right: solid var(--border-size-2) var(--gray-4);
+  }
 
-	header {
-		padding: var(--size-2);
-		font-weight: bold;
-		color: var(--blue-8);
-		border-bottom: solid var(--size-1) var(--blue-2);
-	}
-	header em {
-		font-style: normal;
-		color: var(--blue-4);
-	}
+  header {
+    padding: var(--size-2);
+    font-weight: bold;
+    color: var(--blue-8);
+    border-bottom: solid var(--size-1) var(--blue-2);
+  }
+  header em {
+    font-style: normal;
+    color: var(--blue-4);
+  }
 
-	footer {
-		position: fixed;
-		bottom: 0px;
-		padding: var(--size-2);
-		z-index: 10;
-		background: var(--gray-4);
-		width: 100vw;
-		font-family: var(--font-mono);
-	}
+  footer {
+    position: fixed;
+    bottom: 0px;
+    padding: var(--size-2);
+    z-index: 10;
+    background: var(--gray-4);
+    width: 100vw;
+    font-family: var(--font-mono);
+  }
 
-	.error {
-		background: var(--red-8);
-		color: var(--gray-1);
-	}
+  .error {
+    background: var(--red-8);
+    color: var(--gray-1);
+  }
 </style>
